c1d0e732166dd9c76e7d24b49989d162a13e8737
AlexiWolf <alexiwolf@pm.me>
2022-02-05 12:43:56 -0600

    Added lock option to the shutdown script

--- scripts/.local/bin/dmenu_shutdown.sh
+++ scripts/.local/bin/dmenu_shutdown.sh
@@ -3,7 +3,8 @@ RESTART_COMMAND="systemctl reboot"
 
 
 function prompt_shutdown {
-    case "$(printf "cancel\nshutdown\nrestart" | dmenu -p "Shutdown")" in
+    case "$(printf "cancel\nlock\nshutdown\nrestart" | dmenu -p "Shutdown")" in
+        [lock]*     ) slock;;
         [shutdown]* ) $SHUTDOWN_COMMAND;;
         [restart]*  ) $RESTART_COMMAND;;
         *           ) exit;;
