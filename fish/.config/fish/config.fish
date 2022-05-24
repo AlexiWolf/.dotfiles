f8ec37502fc7fbcec5e83d7d6b9524b3f09d2891
AlexiWolf <alexiwolf@pm.me>
2022-05-23 23:51:41 -0500

    Added Android SDK to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,4 +3,6 @@ fish_add_path -aP ~/.local/scripts
 fish_add_path -aP ~/.cargo/bin
 fish_add_path -aP /opt/android-sdk/cmdline-tools/latest/bin
 
+export ANDROID_SDK_ROOT=/opt/android-sdk/
+
 starship init fish | source
