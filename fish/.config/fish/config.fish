3a8437247835539f98fea9ccb29a34bcdea0a727
AlexiWolf <alexiwolf@pm.me>
2022-06-15 06:44:41 -0500

    Set `fd` as the command for `fzf`

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,4 +5,7 @@ fish_add_path -aP /opt/android-sdk/cmdline-tools/latest/bin
 
 export ANDROID_SDK_ROOT=/opt/android-sdk/
 
+export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 starship init fish | source
