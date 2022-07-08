a113b4a42e72e7c119b615fc0a94557c8bb9538f
AlexiWolf <alexiwolf@pm.me>
2022-07-08 17:48:11 -0500

    Changed `GEM_HOME` to `~/.gems/`

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 fish_add_path -aP ~/.local/bin
 fish_add_path -aP ~/.local/scripts
 fish_add_path -aP ~/.cargo/bin
+fish_add_path -aP ~/.gems/bin
 fish_add_path -aP /opt/android-sdk/cmdline-tools/latest/bin
 
 export ANDROID_SDK_ROOT=/opt/android-sdk/
@@ -8,7 +9,6 @@ export ANDROID_SDK_ROOT=/opt/android-sdk/
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-export GEM_HOME=~/.local/gems/
-fish_add_path -aP ~/.local/gems/bin
+export GEM_HOME=~/.gems/
 
 starship init fish | source
