b24f96495ddf88d8e619a04961cb4f03a50b57e5
AlexiWolf <alexiwolf@pm.me>
2022-07-08 18:01:36 -0500

    Changed to exporting default `gem/bin` location

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,12 @@
 fish_add_path -aP ~/.local/bin
 fish_add_path -aP ~/.local/scripts
 fish_add_path -aP ~/.cargo/bin
-fish_add_path -aP ~/.gems/bin
+fish_add_path -aP ~/.local/share/gem/ruby/3.0.0/bin
 fish_add_path -aP /opt/android-sdk/cmdline-tools/latest/bin
 
 export ANDROID_SDK_ROOT=/opt/android-sdk/
 
 export FZF_DEFAULT_COMMAND='fd --type f'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
-export GEM_HOME=~/.gems/
 
 starship init fish | source
